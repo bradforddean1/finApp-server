0a339d0af6960ddbfd26dea5d60bfecf0a04db07
Bradford Wilson <bradford.dean.wilson@gmail.com>
2020-12-17 21:36:48 -0500

    Cors troubleshooting

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(helmet());
 // app.use(cors());
 // CORS
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", WWW_ROOT);
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
 	res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
