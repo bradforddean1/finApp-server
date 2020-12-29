74c000a7a431befc919696e8f427da395c633dae
Matthew Day <matt@matthewday.net>
2020-12-29 12:05:52 -0500

    Add supertest-session to global

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,11 @@
 const { assert } = require("chai");
 const supertest = require("supertest");
+const supertestSession = require('supertest-session');
 // const app = require("../src/app");
 // const db = require("../db/connection");
 
 global.assert = assert;
 global.supertest = supertest;
+global.supertestSession = supertestSession;
 // global.app = require("../src/app");
 // global.assert = db;
