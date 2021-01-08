d24408d1ab2b7d605e946e503b84c00aec3acc7a
Bradford Wilson <bradford.dean.wilson@gmail.com>
2021-01-08 10:35:26 -0500

    troubleshoot cookies

--- src/app.js
+++ src/app.js
@@ -51,7 +51,10 @@ app.use(passport.session());
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", CLIENT_ROOT);
 	res.header("Access-Control-Allow-Credentials", "true");
-	res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
 	res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
 	// if (req.method === "OPTIONS") {
 	// 	return res.send(204);
